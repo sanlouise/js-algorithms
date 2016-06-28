d94ce2c4180e41b2265e09529130d6b9295f4bb1
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-28 23:01:48 +0200

    Update reverse_string

--- reverse_string.js
+++ reverse_string.js
@@ -3,11 +3,38 @@ function reverseString(string) {
 	for (var i = string.length; i >= 0; i--) {
 		reversedString += string[i];
 	}
-
 	return reversedString;
 }
-
 console.log(reverseString("Hello there!"));
 
 
+// http://www.thatjsdude.com/interview/js1.html
+function reverse(str){
+  var rtnStr = [];
+  if(!str || typeof str != 'string' || str.length < 2 ) return str;
+  
+  for(var i = str.length-1; i>=0;i--){
+    rtnStr.push(str[i]);
+  }
+  return rtnStr.join('');
+}
 
+
+//Recursive
+function reverse (str) {
+    if (str === "") {
+        return "";
+    } else {
+        return reverse(str.substr(1)) + str.charAt(0);
+    }
+}
+
+console.log(reverse("Hello!"));
+
+//With built-in method
+
+function reverse(str){
+  if(!str || str.length <2) return str;
+  
+  return str.split('').reverse().join('');
+}
