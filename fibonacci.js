bea6fa8e84f38bf43e6bc7ea97d323beb4b89a1e
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-28 20:04:27 +0200

    Add greatest_common_divisor

--- fibonacci.js
+++ fibonacci.js
@@ -3,11 +3,9 @@
 function getFibonacci(num) {
 
 	var fibonacci = [0, 1];
-
 	for (var i = 2; i <= num; i++) {
 		fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
 	}
-
 	return fibonacci[num];
 }
 
---
+++ greatest_common_divisor.js
@@ -0,0 +1,17 @@
+//Find the greatest common divisor of two numbers
+
+function gcd( a, b ) {
+
+	//Convert to positive inetgers
+    if (a < 0) a = -a;
+    if (b < 0) b = -b;
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
+    }
+}
+
+console.log(gcd(22, 66));
