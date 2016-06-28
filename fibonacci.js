0e40eebc13cb050f552eb9b066ca65b3f790a04c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-28 17:27:50 +0200

    Add Fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+// F (0) = 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ...
+
+function getFibonacci(num) {
+
+	var fibonacci = [0, 1];
+
+	for (var i = 2; i <= num; i++) {
+		fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+	}
+
+	return fibonacci[num];
+}
+
+console.log(getFibonacci(1));
