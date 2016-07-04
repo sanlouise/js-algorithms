0165b51ad306397df99d78eae0e779fbc32a2fee
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-04 10:05:41 +0200

    Add recursive make_tree.js

---
+++ recursion/make_tree.js
@@ -0,0 +1,25 @@
+let categories = [
+  {id: 'animals', 'parent': null },
+  {id: 'mammals', 'parent': 'animals' },
+  {id: 'horses', 'parent': 'mammals' },
+  {id: 'dogs', 'parent': 'mammals' },
+  {id: 'jack russell', 'parent': 'dogs' },
+  {id: 'rottweiler', 'parent': 'dogs' },
+  {id: 'appaloosa', 'parent': 'horses' },
+  {id: 'shetland', 'parent': 'horses' }
+];
+
+let makeTree = (categories, parent) => {
+    let node = {}
+    categories
+        .filter(c => c.parent === parent)
+        .forEach(c =>
+            node[c.id] = makeTree(categories, c.id))
+    return node
+}
+
+console.log(
+    JSON.stringify(
+        makeTree(categories, null), 
+        null, 2)
+)
